35ab73333fd09ccc49fcca8c2cff87875fb54fed
JoshuaGale <joshua.gale@my.jcu.edu.au>
2017-08-16 14:02:00 +1000

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,12 @@
 """Joshua Gale"""
+second_letter_name = ""
+name = input("What is your name: ")
+while name == "":
+    print("Please enter a valid name: ")
+    name = input("What is your name: ")
+
+for char in range(len(name)):
+    if char % 2 == 1:
+        second_letter_name += name[char]
+
+print(second_letter_name)
